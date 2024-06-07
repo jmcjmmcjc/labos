d9235dd860f87a736ac92ebbf5598e7b736a9cc5
muzhong-bst <muzhong@bluestacks.com>
2024-06-07 18:04:12 +0800

    ucore: patch lab1 lab2 lab3 codes into lab4

--- ucore_lab/labcodes/lab4/kern/trap/trap.c
+++ ucore_lab/labcodes/lab4/kern/trap/trap.c
@@ -10,6 +10,7 @@
 #include <vmm.h>
 #include <swap.h>
 #include <kdebug.h>
+#include <string.h>
 
 #define TICK_NUM 100
 
@@ -48,6 +49,13 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < 256; ++i)
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+    SETGATE(idt[T_SWITCH_TOK], 1, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -162,6 +170,11 @@ pgfault_handler(struct trapframe *tf) {
 static volatile int in_swap_tick_event = 0;
 extern struct mm_struct *check_mm_struct;
 
+static struct trapframe user_tf = {};
+
+extern void switch_to_userland();
+extern void switch_to_kernelland();
+
 static void
 trap_dispatch(struct trapframe *tf) {
     char c;
@@ -186,19 +199,52 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks++;
+        if (ticks % TICK_NUM == 0) {
+            print_ticks();
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
         cprintf("serial [%03d] %c\n", c, c);
+        if (c == '3') {
+            switch_to_userland();
+        }
+        if (c == '0') {
+            switch_to_kernelland();
+        }
         break;
     case IRQ_OFFSET + IRQ_KBD:
         c = cons_getc();
         cprintf("kbd [%03d] %c\n", c, c);
+        if (c == '3') {
+            switch_to_userland();
+        }
+        if (c == '0') {
+            switch_to_kernelland();
+        }
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
+        if (tf->tf_cs != USER_CS) {
+            user_tf = *tf;
+            user_tf.tf_cs = USER_CS;
+            user_tf.tf_ds = user_tf.tf_es = user_tf.tf_ss = USER_DS;
+            user_tf.tf_esp = (uint32_t)tf + sizeof(struct trapframe) - 8;
+            user_tf.tf_eflags |= FL_IOPL_MASK | FL_IF;
+            *((uint32_t *)tf - 1) = (uint32_t)&user_tf;
+        }
+        break;
     case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
+        if (tf->tf_cs != KERNEL_CS) {
+            struct trapframe *kern_tf;
+            tf->tf_cs = KERNEL_CS;
+            tf->tf_ds = tf->tf_es = KERNEL_DS;
+            tf->tf_eflags &= ~FL_IOPL_MASK;
+            kern_tf = (struct trapframe *)(tf->tf_esp - (sizeof(struct trapframe) - 8));
+            memmove(kern_tf, tf, sizeof(struct trapframe) - 8);
+            *((uint32_t *)tf - 1) = (uint32_t)kern_tf;
+        }
         break;
     case IRQ_OFFSET + IRQ_IDE1:
     case IRQ_OFFSET + IRQ_IDE2:
